bf1053e4552802018e3b90bc5d2e14666a823bef
pritikumari <pritikumarinits@gmail.com>
2024-12-26 12:23:47 +0530

    Cleaned repository history

---
+++ config/dev.js
@@ -0,0 +1,11 @@
+module.exports = {
+  googleClientID: process.env.GOOGLE_CLIENT_ID,
+  googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  mongoURI: process.env.MONGO_URI_DEV,
+  cookieKey: process.env.COOKIE_KEY,
+  redisUrl:process.env.REDIS_URL,
+  accessKeyId:process.env.ACCESSKEY_ID,
+  secretAccessKey:process.env.SECRET_ACCESS_KEY
+    
+  
+};
